43c7bebbfbd8e092f50c87fb026b046fa8e51733
Pettersson, Simon <Simon.Pettersson@flir.com>
2019-07-03 14:14:29 +0200

    Add adaptive polling delay based on load, and simple support for "views"

--- include/elm-architecture/elm-architecture.hpp
+++ include/elm-architecture/elm-architecture.hpp
@@ -1,6 +1,8 @@
 #pragma once
+#include <algorithm>
 #include <deque>
 #include <future>
+#include <iostream>
 #include <variant>
 #include <vector>
 
@@ -16,13 +18,18 @@ using command_type = std::variant<std::shared_future<Msg>, Msg>;
 template <typename Model, typename Msg>
 using return_type = std::tuple<Model, std::vector<command_type<Model, Msg>>>;
 
+template <typename Model, typename Msg>
+using view_return_type = std::vector<command_type<Model, Msg>>;
+
 // Start the eventloop with a given list of initial actions to take
-template <typename Model, typename Msg, typename Update>
+template <typename Model, typename Msg, typename Update, typename View = void>
 auto
 start_eventloop(const std::vector<command_type<Model, Msg>>& init = {}) {
     auto                                 model = Model {};
-    std::deque<command_type<Model, Msg>> pending{init.begin(), init.end()};
+    std::deque<command_type<Model, Msg>> pending {init.begin( ), init.end( )};
     std::vector<std::shared_future<Msg>> in_progress;
+    auto                                 sleep_duration_us     = 1024;
+    const auto                           max_sleep_duration_us = sleep_duration_us * 16;
 
     while(pending.size( ) > 0 || in_progress.size( ) > 0) {
         // Step One: Apply all pending events and remove them
@@ -31,26 +38,41 @@ start_eventloop(const std::vector<command_type<Model, Msg>>& init = {}) {
             if(std::holds_alternative<std::shared_future<Msg>>(item)) {
                 in_progress.push_back(std::get<std::shared_future<Msg>>(item));
             } else {
-                const auto& msg             = std::get<Msg>(item);
-                const auto  visitor         = [&model](const auto& msg) { return Update::update(model, msg); };
-                auto [next_model, commands] = std::visit(visitor, msg);
+                const Msg& msg                 = std::get<Msg>(item);
+                const auto update_visitor      = [&model](const auto& msg) { return Update::update(model, msg); };
+                return_type<Model, Msg> result = std::visit(update_visitor, msg);
+                model                          = std::get<Model>(result);
+                const auto& commands           = std::get<std::vector<command_type<Model, Msg>>>(result);
                 std::copy(commands.begin( ), commands.end( ), std::back_inserter(pending));
-                model = next_model;
+                if constexpr(!std::is_same_v<View, void>) {
+                    const auto view_visitor = [&model](const auto& msg) { return View::view(model, msg); };
+                    std::vector<command_type<Model, Msg>> commands = std::visit(view_visitor, msg);
+                    std::copy(commands.begin( ), commands.end( ), std::back_inserter(pending));
+                }
             }
             pending.pop_front( );
         }
 
-        // Step Two: Pause the loop, the only way we get more events now is by polling
-        // until one of the pending events finishes.
-        {
-            for(auto future = in_progress.begin( ); future != in_progress.end( ); ++future) {
-                if(future->wait_for(std::chrono::milliseconds {1}) == std::future_status::ready) {
-                    pending.push_back(future->get( ));
-                    in_progress.erase(future);
-                    break;
-                }
+        // Step Two: Process all the finished IO tasks and push their resulting
+        // messages to the message queue
+        const auto remove_from   = std::remove_if(in_progress.begin( ), in_progress.end( ), [&](auto& future) {
+            const auto status = future.wait_for(std::chrono::microseconds {sleep_duration_us});
+            if(status == std::future_status::ready) {
+                pending.push_back(future.get( ));
+                return true;
             }
-        }
+            return false;
+        });
+        const auto removed_tasks = std::distance(remove_from, in_progress.end( ));
+        in_progress.erase(remove_from, in_progress.end( ));
+
+        // Step Three: Adjust the sleep duration so that on higher loads we
+        // sleep less (increasing throughput), and on lower loads we sleep more
+        // (decreasing CPU load).
+        sleep_duration_us = removed_tasks ? std::max(1, sleep_duration_us / 2)
+                                          : std::min(sleep_duration_us * 2, max_sleep_duration_us);
+
+        std::this_thread::yield( );
     }
 }
 } // namespace elm_architecture
